2a4bdd741cd8af80e7bead475a7bd474ccc6cdba
nowalor <no@walor.io>
2023-08-09 21:13:46 +0200

    feat: use language query and setup template

--- src/components/languages/language-list.svelte
+++ src/components/languages/language-list.svelte
@@ -1,4 +1,22 @@
 <script lang="ts">
+	import { GetLanguages } from '$lib/graphql/queries';
+	import { error } from '@sveltejs/kit';
+	import { getContextClient, queryStore } from '@urql/svelte';
+
+	const languages = queryStore({
+		client: getContextClient(),
+		query: GetLanguages
+	});
 </script>
 
-<div />
+<div>
+	{#if $languages.fetching}
+		...fetching
+	{:else if $languages.error}
+		TODO handle error..
+	{:else}
+		{#each $languages.data.languages as language}
+			<div>{language.name}</div>
+		{/each}
+	{/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import { CodeBlock } from '@skeletonlabs/skeleton';
 	import SnippetList from '../components/snippets/snippet-list.svelte';
+	import LanguageList from '../components/languages/language-list.svelte';
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
+	<LanguageList />
 	<SnippetList />
 	<CodeBlock code="<div>hello<div/>" />
 </div>
