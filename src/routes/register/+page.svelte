875b65a57598f5b2fa78601737abbadeebe05824
nowalor <no@walor.io>
2023-08-09 22:01:38 +0200

    feat: complete register page UI

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,8 +1,43 @@
 <script lang="ts">
 </script>
 
-<div>
+<div class="container h-full mx-auto flex justify-center items-center">
 	<div class="card">
-		<form>d</form>
+		<header class="card-header h2 text-center pt-2">Register</header>
+
+		<section class="p-4">
+			<div>
+				<blockquote class="blockquote">
+					Sign up in 30 seconds and enjoy all the features of snipppets
+				</blockquote>
+			</div>
+			<div class="mt-2">
+				<label class="label">
+					<span>Email*</span>
+					<input class="input" type="email" placeholder="example@example.com" />
+				</label>
+			</div>
+			<div class="mt-2">
+				<label class="label">
+					<span>Username*</span>
+					<input class="input" type="email" placeholder="username" />
+				</label>
+			</div>
+			<div class="mt-2">
+				<label class="label">
+					<span>Password*</span>
+					<input class="input" type="password" placeholder="*******" />
+				</label>
+			</div>
+			<div class="mt-2">
+				<label class="label">
+					<span>Confirm password*</span>
+					<input class="input" type="password" placeholder="*******" />
+				</label>
+			</div>
+			<div class="mt-8">
+				<button type="button" class="btn btn-lg variant-filled w-full">Register</button>
+			</div>
+		</section>
 	</div>
 </div>
